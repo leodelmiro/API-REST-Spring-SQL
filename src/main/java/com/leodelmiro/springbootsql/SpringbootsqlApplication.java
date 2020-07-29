5b2878162a1c5c772839e726b573f0fdea596902
Leonardo Delmiro <leodelmiroms@gmail.com>
2020-07-29 18:15:44 -0300

    First Commit

---
+++ src/main/java/com/leodelmiro/springbootsql/SpringbootsqlApplication.java
@@ -0,0 +1,13 @@
+package com.leodelmiro.springbootsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootsqlApplication.class, args);
+	}
+
+}
